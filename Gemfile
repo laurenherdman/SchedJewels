9a430fe2c3b81911d370a4bc8d749beb5904c71b
stephthi <nstephanie2@gmail.com>
2015-12-16 15:02:09 -0500

    Calendar API is now responding, updated gemfile.

--- Gemfile
+++ Gemfile
@@ -23,7 +23,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-gem 'google-api-client'
+gem 'google-api-client', '0.8.6'
+gem 'googleauth'
+
 gem 'omniauth'
 gem 'omniauth-google-oauth2'
 gem 'devise'
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,23 +8,9 @@ class EventsController < ApplicationController
      @event = Event.new(event_params)
 
      if @event.save
-
-       render :nothing => true
-       google_event = { 'summary' => @event.title, 'description' => @event.description, 'location' => @event.location, 'start' => {'dateTime' => @event.start_time}, 'end' => {'dateTime' => @event.end_time} }
-
-       client = Google::APIClient.new
-       client.authorization.access_token = current_user.token
-       service = client.discovered_api('calendar', 'v3')
-
-       result = client.execute(:api_method => service.events.insert,
-                               :parameters => {'calendarId' => 'schedjewelsapp@gmail.com', 'sendNotifications' => true},
-                               :body => JSON.dump(google_event),
-                               :headers => {'Content-Type' => 'application/json'})
-
-       @event.google_event_id = result.data.id
-       @event.save
+      @event.to_google_event(current_user)
      else
-       render :nothing => true
+       render :new
      end
    end
 
