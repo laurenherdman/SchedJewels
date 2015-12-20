e95a380a20aede6c9decdbb520d0367aed2ebd98
stephthi <nstephanie2@gmail.com>
2015-12-20 16:14:21 -0500

    Added static pages and rerouted home/login

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,10 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/home' => 'staticpages#home'
+  get '/signin' => 'staticpages#signin'
 
-
+  root 'staticpages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
