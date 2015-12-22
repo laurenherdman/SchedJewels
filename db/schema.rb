d49230b4dac9af037e9faefab8a48fb25b5aba1f
stephthi <nstephanie2@gmail.com>
2015-12-22 18:00:09 -0500

    Stash schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,10 @@ ActiveRecord::Schema.define(version: 20151222201226) do
     t.string   "title"
     t.text     "description"
     t.string   "location"
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.date     "start_date"
+    t.time     "start_time"
+    t.date     "end_date"
+    t.time     "end_time"
     t.integer  "user_id"
   end
 
@@ -51,12 +53,13 @@ ActiveRecord::Schema.define(version: 20151222201226) do
     t.string   "title"
     t.text     "description"
     t.string   "location"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.integer  "owner_id"
-    t.integer  "event_id"
-    t.time     "start_time"
     t.date     "start_date"
-    t.time     "end_time"
     t.date     "end_date"
+    t.integer  "event_id"
+    t.string   "time_zone"
   end
 
   create_table "proposals_users", force: :cascade do |t|
@@ -67,11 +70,12 @@ ActiveRecord::Schema.define(version: 20151222201226) do
   create_table "timeslots", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "proposal_id"
     t.time     "start_time"
     t.date     "start_date"
     t.time     "end_time"
     t.date     "end_date"
+    t.integer  "proposal_id"
+    t.string   "time_zone"
   end
 
   create_table "users", force: :cascade do |t|
