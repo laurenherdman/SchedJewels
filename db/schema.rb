51125aa5ad1a2057cefe26c232849121735bdd56
stephthi <nstephanie2@gmail.com>
2016-01-06 17:25:43 -0500

    Fixed migration issues with start/end date_time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106202122) do
+ActiveRecord::Schema.define(version: 20160106203634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(version: 20160106202122) do
     t.string   "title"
     t.text     "description"
     t.string   "location"
-    t.datetime "start_time"
-    t.datetime "end_time"
     t.integer  "user_id"
+    t.datetime "start_date_time"
+    t.datetime "end_date_time"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -75,12 +75,13 @@ ActiveRecord::Schema.define(version: 20160106202122) do
     t.string   "title"
     t.text     "description"
     t.string   "location"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.integer  "owner_id"
-    t.integer  "event_id"
-    t.time     "start_time"
     t.date     "start_date"
-    t.time     "end_time"
     t.date     "end_date"
+    t.integer  "event_id"
+    t.string   "time_zone"
   end
 
   create_table "proposals_users", force: :cascade do |t|
@@ -91,11 +92,12 @@ ActiveRecord::Schema.define(version: 20160106202122) do
   create_table "timeslots", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "proposal_id"
     t.time     "start_time"
     t.date     "start_date"
     t.time     "end_time"
     t.date     "end_date"
+    t.integer  "proposal_id"
+    t.string   "time_zone"
   end
 
   create_table "users", force: :cascade do |t|
