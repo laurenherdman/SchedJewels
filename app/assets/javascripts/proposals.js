7832b6b3ed665653bff21ad229a82f17e617108e
Colin Grenville <colingrenville0@gmail.com>
2016-01-07 14:29:42 -0500

    fix some spacing, change background to blurred image, navbar font media queries

--- app/assets/javascripts/proposals.js
+++ app/assets/javascripts/proposals.js
@@ -1,3 +1,9 @@
 $(document).on('ready page:load', function() {
+	var timeslotNumber = 0
 	$('.vote-submit').on('click', function() { $(this).hide() } );
+	$('.time-button').on('click', function() { 
+		timeslotNumber += 1
+		console.log(timeslotNumber);
+		$( "<hr>" ).append( ".timeslot-js-trig" );
+	} );
 });
