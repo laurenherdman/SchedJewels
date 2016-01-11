fc4c667a44c092347af6709885e7daff46058116
Colin Grenville <colingrenville0@gmail.com>
2016-01-11 14:17:22 -0500

    add transitions, change color scheme

--- app/assets/javascripts/proposals.js
+++ app/assets/javascripts/proposals.js
@@ -7,4 +7,7 @@ $(document).on('ready page:load', function() {
 	$('#add-attendee-js').click();
 	$('#add-time-js').click();
 
+	$('.all-proposals').hide().fadeIn(1000);
+	$('.form-block').hide().fadeIn(1000);
+
 });
